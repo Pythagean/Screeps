aa3ea7a63b49ed4c84db94539eefc9da28f7b08d
Pythagean <jeremysto@gmail.com>
2016-11-16 15:17:52 +1300

    Added repairer role

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleRepairer = require('role.repairer');
 var balanceRoles = require('balanceRoles');
 
 module.exports.loop = function () {
@@ -8,6 +9,7 @@ module.exports.loop = function () {
   var harvestersNumber = 2,
     buildersNumber = 2,
     upgradersNumber = 2,
+    repairersNumber = 0,
     roleUpperLimit = 6;
     
     //Make harvesters if running low on energy
@@ -20,6 +22,10 @@ module.exports.loop = function () {
     //Remove builders if nothing to build
     if (Game.rooms['sim'].find(FIND_CONSTRUCTION_SITES) == 0){
       buildersNumber = 0;
+    }
+    //Add repariers if there are structures in need of repair
+    if (Game.rooms['sim'].find(FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax}).length > 0){
+      repairersNumber = 2;
     }
     
     
@@ -49,6 +55,7 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
 
     //console.log('harvesters: ' + harvesters.length + '/' + harvestersNumber + '|builders: ' + builders.length + '/' + buildersNumber+'|upgraders: ' + upgraders.length + '/' + upgradersNumber)
     
@@ -56,6 +63,7 @@ module.exports.loop = function () {
     balanceRoles.run(harvesters, harvestersNumber, 'harvester')
     balanceRoles.run(builders, buildersNumber, 'builder')
     balanceRoles.run(upgraders, upgradersNumber, 'upgrader')
+    balanceRoles.run(repairers, repairersNumber, 'repairer')
     
     
     for(var name in Game.creeps) {
@@ -68,6 +76,9 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
+        }
+        if(creep.memory.role == 'repairer') {
+            roleRepairer.run(creep);
         }
     }
 }
